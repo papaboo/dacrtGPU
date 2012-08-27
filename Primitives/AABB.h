1cea0751b2c632cca20762be6b86e80bd8349bd9
papaboo <asgerhoedt@gmail.com>
2012-08-27 17:05:07 +0200

    Added a ToString, InvertedSize and ClosestIntersection

--- Primitives/AABB.h
+++ Primitives/AABB.h
@@ -9,7 +9,13 @@
 #ifndef _GPU_DACRT_AABB_H_
 #define _GPU_DACRT_AABB_H_
 
+#include <Utils/ToString.h>
+#include <Utils/Utils.h>
+
 #include <cutil_math.h>
+
+#include <sstream>
+#include <iomanip>
 
 struct AABB {
     float3 min;
@@ -29,10 +35,44 @@ struct AABB {
     }
 
     __host__ __device__
-    inline float3 Extends() const {
+    inline float3 Size() const {
         return max - min;
     }
 
+    __host__ __device__
+    inline float3 InvertedSize() const {
+        const float3 size = Size();
+        return make_float3(1.0f / size.x, 1.0f / size.y, 1.0f / size.z);
+    }
+
+    __host__ __device__
+    inline bool ClosestIntersection(const float3& rayOrigin, const float3& rayDir, float& tHit) const {
+        float3 minTs = (min - rayOrigin) / rayDir;
+        float3 maxTs = (max - rayOrigin) / rayDir;
+        
+        float nearT = Min(minTs.x, maxTs.x);
+        nearT = Max(nearT, Min(minTs.y, maxTs.y));
+        nearT = Max(nearT, Min(minTs.z, maxTs.z));
+        
+        float farT = Max(minTs.x, maxTs.x);
+        farT = Min(farT, Max(minTs.y, maxTs.y));
+        farT = Min(farT, Max(minTs.z, maxTs.z));
+     
+        tHit = nearT;
+
+        return nearT <= farT && 0.0f <= farT;
+    }
+
+    std::string ToString() const {
+        std::ostringstream out;
+        out << std::fixed << std::setprecision(2) << "[min: " << min << ", max: " << max << "]";
+        return out.str();
+    }
+    
 };
+
+inline std::ostream& operator<<(std::ostream& s, const AABB& aabb){
+    return s << aabb.ToString();
+}
 
 #endif
