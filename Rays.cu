408022cb5f264b664c7a56f9c15dd35b0976575a
papaboo <asgerhoedt@gmail.com>
2012-10-11 11:39:58 +0200

    Rays are created using a thread-local random seeded with a global seed + a hash of the thread id.

--- Rays.cu
+++ Rays.cu
@@ -8,6 +8,8 @@
 
 #include <Rays.h>
 
+#include <Meta/CUDA.h>
+#include <Utils/Random.h>
 #include <Utils/ToString.h>
 
 #include <thrust/host_vector.h>
@@ -68,58 +70,78 @@ struct DeviceCreateRays {
     }
 };
 
-Rays::Rays(const int width, const int height, const int sqrtSamples) {
-    // Random can be replaced by a random seed in a global var plus some
-    // permutation of block and thread x's and y's.
-    ///  -- or --
-    // use http://http.developer.nvidia.com/GPUGems3/gpugems3_ch37.html
+// TODO Adopted from a linearly indexed thrust operator. Could probably be speed
+// up as a 2D kernel.
+__global__
+void CreateRaysKernel(const int width, const int height, const int sqrtSamples,
+                      const float3 cx, const float3 cy,
+                      const unsigned int seed,
+                      float4* origins, float4* directions) {
+
+    const unsigned int index = threadIdx.x + blockDim.x * blockIdx.x;
+    if (index >= width * height * sqrtSamples * sqrtSamples) return;
+
+    // Reverse: int index = (x + y * width) * samples + subX + subY * sqrtSamples;
+    const unsigned short subX = index % sqrtSamples;
+    const unsigned short subY = (index / sqrtSamples) % sqrtSamples;
+    const int lowResIndex = index / (sqrtSamples * sqrtSamples);
+    const unsigned short x = lowResIndex % width;
+    const unsigned short y = lowResIndex / width;
     
+    const float3 camOrigin = make_float3(50.0f, 52.0f, 295.6f);
+    const float3 camDir = make_float3(0.0f, -0.042612f,-1.0f);
+
+    Random rand = Random::Create1D(seed);
+
+    const float r1 = 2.0f * rand.NextFloat01();
+    const float dx = r1 < 1.0f ? sqrt(r1) - 1.0f : 1.0f - sqrt(2.0f - r1);
+    const float r2 = 2.0f * rand.NextFloat01();
+    const float dy = r2 < 1.0f ? sqrt(r2) - 1.0f : 1.0f - sqrt(2.0f - r2);
+    
+    const float3 rayDir = cx * (((subX + 0.5f + dx) / sqrtSamples + x) / width - 0.5f) 
+        + cy * (((subY + 0.5f + dy) / sqrtSamples + y) / height - 0.5f) + camDir;
+    
+    const float3 rayOrigin = camOrigin + 130.0f * rayDir;
+    origins[index] = make_float4(rayOrigin, index);
+    directions[index] = make_float4(normalize(rayDir), 0.0f);
+}
+
+Rays::Rays(const int width, const int height, const int sqrtSamples) {
     const int size = width * height * sqrtSamples * sqrtSamples;
     origins = thrust::device_vector<float4>(size);
     axisUVs = thrust::device_vector<float4>(size);
-    
-    const float3 camOrigin = make_float3(50.0f, 52.0f, 295.6f);
+
     const float3 camDir = make_float3(0.0f, -0.042612f,-1.0f);
     
     const float3 cx = make_float3(width * 0.5135f / height, 0, 0);
     const float3 cy = normalize(cross(cx, camDir)) * 0.5135f;
 
-    // Generate random numbers
-    thrust::host_vector<float2> host_random(size);
-    thrust::generate(host_random.begin(), host_random.end(), RandomFloat2);
-    thrust::device_vector<float2> random = host_random;
-
-    DeviceCreateRays deviceCreateRays(width, height, sqrtSamples, cx, cy);
-    thrust::transform(random.begin(), random.end(), thrust::counting_iterator<unsigned int>(0), 
-                      Begin(), deviceCreateRays);
+    bool useHostRandom = false;
+    if (useHostRandom) {
+        // Generate random numbers
+        thrust::host_vector<float2> host_random(size);
+        thrust::generate(host_random.begin(), host_random.end(), RandomFloat2);
+        thrust::device_vector<float2> random = host_random;
+        
+        DeviceCreateRays deviceCreateRays(width, height, sqrtSamples, cx, cy);
+        thrust::transform(random.begin(), random.end(), thrust::counting_iterator<unsigned int>(0), 
+                          Begin(), deviceCreateRays);
+    } else {
+        struct cudaFuncAttributes funcAttr;
+        cudaFuncGetAttributes(&funcAttr, CreateRaysKernel);
+        unsigned int blocksize = funcAttr.maxThreadsPerBlock > 256 ? 256 : funcAttr.maxThreadsPerBlock;
+        unsigned int blocks = (size / blocksize) + 1;
+        std::cout << "CreateRaysKernel<<<" << blocks << ", " << blocksize << ">>>" << std::endl;
+        CreateRaysKernel<<<blocks, blocksize>>>(width, height, sqrtSamples, cx, cy, rand(),
+                                                RawPointer(origins), RawPointer(axisUVs));
+        CHECK_FOR_CUDA_ERROR();
+    }
 
     representation = RayRepresentation;
 
-    /*    
-    for (int y = 0; y < height; y++){
-        unsigned short Xi[3] = {0, 0, y*y*y};
-        for (unsigned short x = 0; x < width; x++) {
-            
-            // subpixel grid
-            for (int subY = 0; subY < sqrtSamples; ++subY)
-                for (int subX = 0; subX < sqrtSamples; ++subX) {
-                    // Samples
-                    double r1 = 2 * erand48(Xi);
-                    float dx = r1 < 1 ? sqrt(r1) - 1 : 1 - sqrt(2 - r1);
-                    double r2 = 2 * erand48(Xi);
-                    float dy = r2 < 1 ? sqrt(r2) - 1: 1 - sqrt(2 - r2);
-                    
-                    float3 rayDir = cx * (((subX + 0.5 + dx) / sqrtSamples + x) / width - 0.5) 
-                        + cy * (((subY + 0.5 + dy) / sqrtSamples + y) / height - 0.5) + camDir;
-                    unsigned int index = (x + y * width) * sqrtSamples * sqrtSamples + subX + subY * sqrtSamples;
-                    
-                    float3 origin = camOrigin + rayDir * 130;
-                    origins[index] = make_float4(origin.x, origin.y, origin.z, index);
-                    axisUVs[index] = make_float4(rayDir, 0.0f);
-                }
-        }
-    }
-    */    
+    std::cout << "Rays:" << std::endl;
+    for (int i = 0; i < 10; ++i)
+        std::cout << i << ": " << GetAsRay(i+size/2) << std::endl;
 }
 
 struct RaysToHyperRays {
